5cd46a1f49ba28d399821512c71395906d1b2eed
turtlecode <85156399+turtlecode@users.noreply.github.com>
2022-02-14 17:48:41 +0300

    How to Scroll Back to Top Button - Javascript
    How to Scroll Back to Top Button - Javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+var button = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
